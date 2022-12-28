8ab03ab51f6afd5781d95c123834adf22beba24c
Michał Bryłka <bmichal76@gmail.com>
2022-12-28 21:59:24 +0100

    streams

--- src/main/java/pl/javastart/streamstask/StreamsTask.java
+++ src/main/java/pl/javastart/streamstask/StreamsTask.java
@@ -1,10 +1,8 @@
 package pl.javastart.streamstask;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamsTask {
 
@@ -38,22 +36,67 @@ public class StreamsTask {
 
     // metoda powinna zwracać listę kobiet (sprawdzając, czy imię kończy się na "a")
     Collection<User> findWomen(Collection<User> users) {
-        throw new RuntimeException("Not implemented");
+        validateUserCollection(users);
+        return users.stream()
+                .filter(this::isWomen)
+                .collect(Collectors.toList());
     }
 
     // metoda powinna zwracać średni wiek mężczyzn (sprawdzając, czy imię nie kończy się na "a")
     Double averageMenAge(Collection<User> users) {
-        throw new RuntimeException("Not implemented");
+        validateUserCollection(users);
+        return  users.stream()
+                .filter(this::isMen)
+                .map(user -> (double) user.getAge())
+                .collect(Collectors.averagingDouble(value -> value));
     }
 
     // metoda powinna zwracać wydatki zgrupowane po ID użytkownika
     Map<Long, List<Expense>> groupExpensesByUserId(Collection<User> users, List<Expense> expenses) {
-        throw new RuntimeException("Not implemented");
+        validateCollections(users, expenses);
+        return users.stream()
+                .collect(Collectors.toMap(User::getId, user -> getExpensesForUser(user, expenses)));
     }
 
     // metoda powinna zwracać wydatki zgrupowane po użytkowniku
     // podobne do poprzedniego, ale trochę trudniejsze
     Map<User, List<Expense>> groupExpensesByUser(Collection<User> users, List<Expense> expenses) {
-        throw new RuntimeException("Not implemented");
+        validateCollections(users, expenses);
+        return users.stream()
+                .collect(Collectors.toMap(user -> user, user -> getExpensesForUser(user, expenses)));
+    }
+
+    private boolean isWomen(User user) {
+        if (user != null) {
+            String name = user.getName();
+            char[] chars = name.toCharArray();
+            int size = chars.length;
+            if (size > 0) {
+                return chars[size - 1] == 'a';
+            }
+        }
+        return false;
+    }
+
+    private boolean isMen(User user) {
+        return !isWomen(user);
+    }
+
+    private void validateUserCollection(Collection<User> users) {
+        if (users == null) {
+            throw new IllegalArgumentException("Niezainicjalizowana kolekcja dla użytkowników");
+        }
+    }
+
+    private void validateCollections(Collection<User> users, List<Expense> expenses) {
+        if (users == null || expenses == null) {
+            throw new IllegalArgumentException("Niezainicjalizowane kolekcje dla grupowania");
+        }
+    }
+
+    private List<Expense> getExpensesForUser(User user, List<Expense> expense) {
+        return expense.stream()
+                .filter(exp -> exp.getUserId().equals(user.getId()))
+                .collect(Collectors.toList());
     }
 }
